7d23e0547611520945617a177a5b91e91b68a9f2
Rina <rinahan4146@gmail.com>
2021-03-21 13:23:14 -0700

    first commit Rina

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import MainPage from './pages/MainPage'
 import FeedPage from 'pages/FeedPage'
 import PostPage from 'pages/PostPage'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.scss'
 
 function App() {
@@ -16,17 +11,14 @@ function App() {
       {/* <FeedPage/> */}
       {/* <PostPage /> */}
 
-
       <Router>
         <Switch>
-          <Route path="/MainPage" exact component={MainPage}/>
-          <Route path="/" exact component={FeedPage}/>
+          <Route path='/MainPage' exact component={MainPage} />
+          <Route path='/' exact component={FeedPage} />
           {/* <Route path="/FeedPage" component={FeedPage}/> */}
-          <Route path="/PostPage" component={PostPage}/>
+          <Route path='/PostPage' component={PostPage} />
         </Switch>
       </Router>
-
-
     </div>
   )
 }
