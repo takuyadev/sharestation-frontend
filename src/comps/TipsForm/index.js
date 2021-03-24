e34aea5c489cefc70128fdaac7a63c9d8ca5de3c
Rina <rinahan4146@gmail.com>
2021-03-24 05:35:44 -0700

    fixed bugs

--- src/comps/TipsForm/index.js
+++ src/comps/TipsForm/index.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import Avatar from "comps/Avatar";
-import SmallBtn from "comps/Buttons/SmallBtn";
-import CircleBtn from "comps/Buttons/CircleBtn";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import Avatar from 'comps/Avatar'
+import SmallBtn from 'comps/Buttons/SmallBtn'
+import CircleBtn from 'comps/Buttons/CircleBtn'
+import { Link } from 'react-router-dom'
 
 const App = styled.div`
   width: 100%;
@@ -11,52 +11,53 @@ const App = styled.div`
   flex-direction: column;
   position: fixed;
   bottom: 0;
-`;
+`
 const Container = styled.div`
   width: 100%;
   background-color: #fff;
   border-radius: 40px 40px 0px 0px;
   box-shadow: 0px 4px 54.3656px rgba(0, 0, 0, 0.4);
   padding-top: 5px;
-`;
+`
 const UpDownBtnCont = styled.div`
   margin: auto;
-`;
+`
 const UpBtn = styled.div`
-  display: ${(props) => (props.expanded ? "none" : "flex")};
-`;
+  display: ${(props) => (props.expanded ? 'none' : 'flex')};
+`
 const DownBtn = styled.div`
-  display: ${(props) => (props.expanded ? "flex" : "none")};
-`;
+  display: ${(props) => (props.expanded ? 'flex' : 'none')};
+`
 const MarginCont = styled.div`
   margin: 20px auto 20px;
   width: 80%;
-`;
+`
 const AvatarCont = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-`;
+`
 const IdCont = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   height: 45px;
-`;
+`
 const Id = styled.div`
-  font-size: 17px;
-`;
+  font-size: 14px;
+`
 const Liked = styled.div`
   font-size: 12px;
+  /* font-weight:bold; */
   color: #616161;
-  color:#ff6666;
-`;
+  color: #ff6666;
+`
 const TextCont = styled.div`
   font-size: 14px;
   margin-top: 30px;
-  display: ${(props) => (props.expanded ? "flex" : "none")};
-  /* ${(props) => props.display && "display:" + props.display + ";"} */
-`;
+  display: ${(props) => (props.expanded ? 'flex' : 'none')};
+  /* ${(props) => props.display && 'display:' + props.display + ';'} */
+`
 const CcBtnCont = styled.div`
   display: flex;
   flex-direction: column;
@@ -66,7 +67,7 @@ const CcBtnCont = styled.div`
   margin: 0 20px;
   height: 230px;
   justify-content: space-between;
-`;
+`
 
 const TipsForm = ({
   id,
@@ -80,11 +81,11 @@ const TipsForm = ({
   onChange,
   onLike,
 }) => {
-  const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(false)
 
   useEffect(() => {
-    setExpanded(expand);
-  }, [expand]);
+    setExpanded(expand)
+  }, [expand])
 
   return (
     <App>
@@ -93,58 +94,66 @@ const TipsForm = ({
           <CircleBtn icon='' btnname='loginBtn' />
         </Link> */}
         <CircleBtn
-          icon="icons/icon3.png"
-          btnname="nextBtn"
+          icon='icons/icon3.png'
+          btnname='nextBtn'
           onClick={onChange}
         />
         <CircleBtn
-          icon="icons/icon4.png"
-          btnname="heartBtn"
-          color="#FF5555"
+          icon='icons/icon4.png'
+          btnname='heartBtn'
+          color='#FF5555'
           clicked={btnLiked}
-          onClick={onLike}  
+          onClick={onLike}
         />
-        <Link to="/PostPage">
-          <CircleBtn icon="icons/icon5.png" btnname="addBtn" />
+        <Link to='/PostPage'>
+          <CircleBtn icon='icons/icon5.png' btnname='addBtn' />
         </Link>
       </CcBtnCont>
       <UpDownBtnCont
         onClick={() => {
-          setExpanded(!expanded);
+          setExpanded(!expanded)
         }}
       >
         <UpBtn expanded={expanded}>
-          <SmallBtn icon="icons/icon7.png" />
+          <SmallBtn icon='icons/icon7.png' />
         </UpBtn>
         <DownBtn expanded={expanded}>
-          <SmallBtn icon="icons/icon6.png" />
+          <SmallBtn icon='icons/icon6.png' />
         </DownBtn>
       </UpDownBtnCont>
       <Container>
         <MarginCont>
           <AvatarCont>
             {/* <IdCont> */}
-              <Id>{id}</Id>
+            <Id>{id}</Id>
             {/* </IdCont> */}
-              <Liked>{liked} Liked </Liked>
+            <Liked>{liked} Liked </Liked>
             {/* <Avatar /> */}
           </AvatarCont>
-          <TextCont expanded={expanded} style={{fontWeight:"bold", textTransform:'uppercase'}}>{title}</TextCont>
+          <TextCont
+            expanded={expanded}
+            style={{
+              fontWeight: 'bold',
+              textTransform: 'uppercase',
+              fontSize: '13px',
+            }}
+          >
+            {title}
+          </TextCont>
           <TextCont expanded={expanded}>{text}</TextCont>
         </MarginCont>
       </Container>
     </App>
-  );
-};
+  )
+}
 
 TipsForm.defaultProps = {
-  id: "Rinahan4146",
-  liked: "1.4 liked",
+  id: 'Rinahan4146',
+  liked: '1.4 liked',
   // icon: 'icons/icon6.png',
-  text:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  text:null,
   onExpand: () => {},
   onChange: () => {},
-};
+}
 
-export default TipsForm;
+export default TipsForm
